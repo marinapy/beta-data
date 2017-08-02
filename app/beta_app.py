6d9386fe250b268370a10980996310f4ef1c75d5
Marina Foglietta Tereo <mft274@stern.nyu.edu>
2017-08-02 18:35:05 -0400

    Confirm excluding missing email addresses

--- app/beta_app.py
+++ app/beta_app.py
@@ -16,4 +16,9 @@ panda_beta_data["Published Date"] = pandas.to_datetime(panda_beta_data['Publishe
 
 range_panda_beta_data = panda_beta_data[panda_beta_data["Published Date"].isin(pandas.date_range('7/13/2017', '7/20/2017'))]
 
+#prospect_email = range_panda_beta_data[(range_panda_beta_data["Prospect email "] != 'n/a')]
+#prospect_email_new = prospect_email[(prospect_email["Prospect email "] != 'not in WR, FF or CRM')]
+
+### Excluding missing email addresses
+range_panda_beta_data = range_panda_beta_data[(range_panda_beta_data['Prospect email '].str.contains('@', regex=True) == True)]
 range_panda_beta_data.to_csv('output.csv', columns = header)
