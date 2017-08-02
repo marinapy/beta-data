3cd77b9e06947306bee2f3be2896981c8469eb51
Marina Foglietta Tereo <mft274@stern.nyu.edu>
2017-08-02 18:14:25 -0400

    Confirm tranform Published date to date time

--- app/beta_app.py
+++ app/beta_app.py
@@ -1,8 +1,17 @@
 import pandas
+import numpy as np
 
 panda_beta_data = pandas.read_csv("~/Desktop/beta-data/app/BetaTracker.csv")
 #print (panda_beta_data.head())
 
 ### Selecting Columns from Original Data to Keep
 header = ["Prospect Name", "Prospect email ", "FR Name", "Published Date"]
+
+### Format 'Published Date' as date_time
+
+panda_beta_data["Published Date"] = panda_beta_data["Published Date"].replace('SV', np.nan)
+panda_beta_data["Published Date"] = pandas.to_datetime(panda_beta_data['Published Date'], format="%m/%d/%Y")
+
 panda_beta_data.to_csv('output.csv', columns = header)
+
+### Defining the data range
