f599de4f9bf449d8cb611f76ab1dc9be1e8a0dd7
Marina Foglietta Tereo <mft274@stern.nyu.edu>
2017-08-02 18:06:41 -0400

    Confirm writing new CSV

--- app/beta_app.py
+++ app/beta_app.py
@@ -1,5 +1,8 @@
 import pandas
 
 panda_beta_data = pandas.read_csv("~/Desktop/beta-data/app/BetaTracker.csv")
+#print (panda_beta_data.head())
 
-print (panda_beta_data.head()) 
+### Selecting Columns from Original Data to Keep
+header = ["Prospect Name", "Prospect email ", "FR Name", "Published Date"]
+panda_beta_data.to_csv('output.csv', columns = header)
